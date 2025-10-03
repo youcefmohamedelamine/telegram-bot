a709ee176a5025e84a066e4cdd781b29dbadf26a
youcefmohamedelamine <abdaka1345@gmail.com>
2025-10-03 09:41:20 -0700

    Update bot2.py

--- bot2.py
+++ bot2.py
@@ -1,4 +1,5 @@
 import logging
+import os
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Application, CommandHandler, ContextTypes, CallbackQueryHandler
 
