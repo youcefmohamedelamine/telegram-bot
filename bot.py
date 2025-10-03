907eb99cff3dbd963768628bfb2c4354f7de1ef3
youcefmohamedelamine <abdaka1345@gmail.com>
2025-10-03 03:03:23 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,6 +12,7 @@ from telegram.ext import (
     PreCheckoutQueryHandler,
 )
 
+
 logging.basicConfig(level=logging.INFO,
                     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
