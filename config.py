db7443e4f0d6444895c1a692d48cec32e80c07cd
youcefmohamedelamine <abdaka1345@gmail.com>
2025-10-05 13:37:12 -0700

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-# Загружаем переменные окружения из .env файла
-load_dotenv()
-
-# Получаем значения переменных окружения
-TOKEN = os.getenv("TOKEN") 
-DATABASE = os.getenv('DATABASE')
